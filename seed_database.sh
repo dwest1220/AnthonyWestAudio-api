fe8576ebe1ae39a446473f67b4833c4308504481
David West <davidpwest1220@gmail.com>
2025-08-04 16:00:41 -0500

    Initial commit

---
+++ seed_database.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm db.sqlite3
+rm -rf ./audioapi/migrations
+python3 manage.py migrate
+python3 manage.py makemigrations audioapi
+python3 manage.py migrate audioapi
+python3 manage.py loaddata users
+python3 manage.py loaddata tokens
+
