e783452a241f7edff2f0ff7c59e0bd792ca31f2f
David West <davidpwest1220@gmail.com>
2025-08-13 16:06:18 -0500

    Added ability to register new users

--- audioapi/views/__init__.py
+++ audioapi/views/__init__.py
@@ -3,3 +3,4 @@ from .users import UserView
 from .booking import BookingView
 from .bookingstaff import BookingStaffView
 from .staff import StaffView
+from .register import register_view
--- audioproject/urls.py
+++ audioproject/urls.py
@@ -13,6 +13,7 @@ router.register(r'bookings', BookingView, basename='booking')
 router.register(r'booking-staff', BookingStaffView, basename='booking-staff')
 
 urlpatterns = [
+    path('auth/register/', register_view, name='register'),
     path('auth/login/', login_view, name='login'),
     path('auth/logout/', logout_view, name='logout'),
     path('auth/user/', user_profile_view, name='user-profile'),
